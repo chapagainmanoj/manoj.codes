3e73ac25958bdb11766b77351986d25d5180db92
Manoj <chapagainmanoj35@gmail.com>
2024-02-09 12:29:58 -0500

    css not working

--- app/blog/[slug]/header.tsx
+++ app/blog/[slug]/header.tsx
@@ -54,7 +54,7 @@ export const Header: React.FC<Props> = ({ project }) => {
 			>
 				<div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
 					<div className="flex justify-between gap-8">
-						<Link target="_blank" href="https://twitter.com/chronark_">
+						<Link target="_blank" href="https://twitter.com/__man0j">
 							<Twitter
 								className={`w-6 h-6 duration-200 hover:font-medium ${
 									isIntersecting
@@ -63,7 +63,7 @@ export const Header: React.FC<Props> = ({ project }) => {
 								} `}
 							/>
 						</Link>
-						<Link target="_blank" href="https://github.com/chronark">
+						<Link target="_blank" href="https://github.com/chapagainmanoj">
 							<Github
 								className={`w-6 h-6 duration-200 hover:font-medium ${
 									isIntersecting
--- next.config.mjs
+++ next.config.mjs
@@ -3,18 +3,18 @@ import { withContentlayer } from "next-contentlayer";
 /** @type {import('next').NextConfig} */
 
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+// const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-let assetPrefix = ''
-let basePath = '/'
+// let assetPrefix = ''
+// let basePath = '/'
 
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+// if (isGithubActions) {
+//   // trim off `<owner>/`
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
+//   assetPrefix = `/${repo}/`
+//   basePath = `/${repo}`
+// }
 
 const nextConfig = {
 	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
@@ -22,8 +22,8 @@ const nextConfig = {
 	experimental: {
 		mdxRs: true,
 	},
-	assetPrefix: assetPrefix,
-	basePath: basePath,
+	// assetPrefix: assetPrefix,
+	// basePath: basePath,
 };
 
 export default withContentlayer(nextConfig);
