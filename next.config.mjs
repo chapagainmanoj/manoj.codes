1fe3382b7f78446e1d3272b22e38c0f97bf19b29
Manoj <chapagainmanoj35@gmail.com>
2024-02-09 02:42:13 -0500

    export out

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,29 @@
 import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
+
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig = {
 	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 	output: 'export',
 	experimental: {
 		mdxRs: true,
 	},
+	assetPrefix: assetPrefix,
+	basePath: basePath,
 };
 
 export default withContentlayer(nextConfig);
