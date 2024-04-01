623230bbd7e202fe26255c1017b94d8477c1bff7
Manoj <chapagainmanoj35@gmail.com>
2024-04-01 12:42:15 -0400

    Few improvements

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -2,12 +2,9 @@
 
 export function Analytics() {
 	const token = process.env.NEXT_PUBLIC_BEAM_TOKEN;
-	if (!token) {
-		return null;
-	}
 	return (
-		<script 
-			src="https://beamanalytics.b-cdn.net/beam.min.js" 
+		<script
+			src="https://beamanalytics.b-cdn.net/beam.min.js"
 			data-token={token}
 			async
 		/>
