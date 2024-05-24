57a54b8f9822870d409eaaf4cd0928e879db6fe6
Manoj <chapagainmanoj35@gmail.com>
2024-05-24 15:46:27 -0400

    Updates

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -9,16 +9,16 @@ import { Article } from "./article";
 export const revalidate = 60;
 export default async function ProjectsPage() {
 
-  const featured = allProjects.find((project) => project.slug === "asynccasbin")!;
+  const featured = allProjects.find((project) => project.slug === "dcode-shapes")!;
+  const top1 = allProjects.find((project) => project.slug === "asynccasbin")!;
   const top2 = allProjects.find((project) => project.slug === "databases-extensions")!;
-  const top3 = allProjects.find((project) => project.slug === "query-by-humming")!;
   const sorted = allProjects
     .filter((p) => p.published)
     .filter(
       (project) =>
         project.slug !== featured.slug &&
-        project.slug !== top2.slug &&
-        project.slug !== top3.slug,
+        project.slug !== top1.slug &&
+        project.slug !== top2.slug
     )
     .sort(
       (a, b) =>
@@ -77,7 +77,7 @@ export default async function ProjectsPage() {
           </Card>
 
           <div className="flex flex-col w-full gap-8 mx-auto border-t border-gray-900/10 lg:mx-0 lg:border-t-0 ">
-            {[top2, top3].map((project) => (
+            {[top1, top2].map((project) => (
               <Card key={project.slug}>
                 <Article project={project} />
               </Card>
@@ -92,7 +92,7 @@ export default async function ProjectsPage() {
               .filter((_, i) => i % 3 === 0)
               .map((project) => (
                 <Card key={project.slug}>
-                  <Article project={project}/>
+                  <Article project={project} />
                 </Card>
               ))}
           </div>
