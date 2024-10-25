606f4a390ff2ff6ac71b414511de8d68ff9803be
Manoj <chapagainmanoj35@gmail.com>
2024-10-25 11:00:53 -0400

    Google analytics added

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -12,6 +12,17 @@ export function Analytics() {
 				async
 			/>
 			<GoogleAnalytics gaId="G-18MSLYGHGM" />
+			<script
+				dangerouslySetInnerHTML={{
+					__html: `
+				window.dataLayer = window.dataLayer || [];
+				function gtag(){dataLayer.push(arguments);}
+				gtag('js', new Date());
+
+				gtag('config', 'G-18MSLYGHGM');
+            	`,
+				}}
+			/>
 		</>
 	);
 }
