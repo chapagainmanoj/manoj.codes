e55fe1c66368329226b49c1b460f8da27d195335
Manoj <chapagainmanoj35@gmail.com>
2024-10-25 10:49:59 -0400

    Google analytics added

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -1,12 +1,17 @@
 "use client";
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 export function Analytics() {
-	const token = process.env.NEXT_PUBLIC_BEAM_TOKEN;
+	const token = "6a077f15-152f-4940-b04e-e143a62b4ca1";
 	return (
-		<script
-			src="https://beamanalytics.b-cdn.net/beam.min.js"
-			data-token="6a077f15-152f-4940-b04e-e143a62b4ca1"
-			async
-		/>
+		<>
+			<script
+				src="https://beamanalytics.b-cdn.net/beam.min.js"
+				data-token={token}
+				async
+			/>
+			<GoogleAnalytics gaId="G-18MSLYGHGM" />
+		</>
 	);
 }
