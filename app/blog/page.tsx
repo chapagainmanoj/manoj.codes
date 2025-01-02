31a521fc4779059aa2b78aee1b22b82a33023d56
Manoj <chapagainmanoj35@gmail.com>
2025-01-02 17:34:39 -0500

    Update order;

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -11,7 +11,8 @@ export default async function ProjectsPage() {
   console.log("allBlogs", allBlogs.map((p) => p.slug));
 
   const featured = allBlogs.find((project) => project.slug === "tainted-kernel")!;
-  const top2 = allBlogs.find((project) => project.slug === "binary-installers")!;
+  // const top2 = allBlogs.find((project) => project.slug === "binary-installers")!;
+  const top2 = allBlogs.find((project) => project.slug === "developers-spectrum")!;
   const top3 = allBlogs.find((project) => project.slug === "first-react-project")!;
   const sorted = allBlogs
     .filter((p) => p.published)
