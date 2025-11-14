fc94e0d64c1c0504c773036bd97db9a49ce8cb33
Manoj <chapagainmanoj35@gmail.com>
2025-11-14 15:28:43 -0500

    Add llm blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -10,7 +10,7 @@ export const revalidate = 60;
 export default async function ProjectsPage() {
   console.log("allBlogs", allBlogs.map((p) => p.slug));
 
-  const featured = allBlogs.find((project) => project.slug === "developers-spectrum")!;
+  const featured = allBlogs.find((project) => project.slug === "fine-tuning-gpt-2")!;
   // const top2 = allBlogs.find((project) => project.slug === "binary-installers")!;
   const top2 = allBlogs.find((project) => project.slug === "tainted-kernel")!;
   const top3 = allBlogs.find((project) => project.slug === "first-react-project")!;
