bd15fd94e99f6c961ec4350b57b184f58b64e8b5
Manoj <4796993+chapagainmanoj@users.noreply.github.com>
2026-09-06 15:47:20 +0545

    Cloud flare inslight added

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -1,15 +1,14 @@
 "use client";
-import { GoogleAnalytics } from "@next/third-parties/google";
+// import { GoogleAnalytics } from "@next/third-parties/google";
 
 export function Analytics() {
-	const token = "6a077f15-152f-4940-b04e-e143a62b4ca1";
 	return (
 		<>
+			{/* Cloudflare Web Analytics */}
 			<script
-				src="https://beamanalytics.b-cdn.net/beam.min.js"
-				data-token={token}
-				async
-			/>
+				type='module'
+				src='https://static.cloudflareinsights.com/beacon.min.js'
+				data-cf-beacon='{"token": "db5fcbdb8601465ab6c5c56724c7f5ff"}'></script>
 			{/* <GoogleAnalytics gaId="G-18MSLYGHGM" />
 			<script
 				dangerouslySetInnerHTML={{
