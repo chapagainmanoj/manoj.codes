3a763b9e8d34529245af386a3509988f49a3c0ae
Manoj <4796993+chapagainmanoj@users.noreply.github.com>
2026-09-07 20:03:45 +0545

    Clean analytics

--- app/components/analytics.tsx
+++ app/components/analytics.tsx
@@ -9,18 +9,6 @@ export function Analytics() {
 				type='module'
 				src='https://static.cloudflareinsights.com/beacon.min.js'
 				data-cf-beacon='{"token": "db5fcbdb8601465ab6c5c56724c7f5ff"}'></script>
-			{/* <GoogleAnalytics gaId="G-18MSLYGHGM" />
-			<script
-				dangerouslySetInnerHTML={{
-					__html: `
-				window.dataLayer = window.dataLayer || [];
-				function gtag(){dataLayer.push(arguments);}
-				gtag('js', new Date());
-
-				gtag('config', 'G-18MSLYGHGM');
-            	`,
-				}}
-			/> */}
 		</>
 	);
 }
